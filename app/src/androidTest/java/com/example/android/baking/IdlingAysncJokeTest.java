67e6cf542d662da9df41d8b774db73a8a4e21a0c
Christopher Thompson <ChristopherErikThompson@Gmail.com>
2019-06-13 13:06:25 -0700

    modified test to check string

--- app/src/androidTest/java/com/example/android/baking/IdlingAysncJokeTest.java
+++ app/src/androidTest/java/com/example/android/baking/IdlingAysncJokeTest.java
@@ -17,7 +17,11 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.containsString;
 
 @RunWith(AndroidJUnit4.class)
 public class IdlingAysncJokeTest {
@@ -42,6 +46,7 @@ public class IdlingAysncJokeTest {
 
         /* Perform click on recycler view position 0 after loading from network */
         onView(withId(R.id.joke_button)).perform(click());
+        onView(withId(R.id.joke_tv)).check(matches(withText(containsString("TEMPORARY HILARIOUS JOKE"))));
     }
 
     @After
