eb2f9f1461dc373623174837a2342583ee00cf9b
Christopher Thompson <ChristopherErikThompson@Gmail.com>
2019-06-17 09:08:45 -0700

    refactor: Changed hardcoded string to flavor string resource
    What: In activity on create banner ad was instantiated with hardcoded string. String is now set with the appropriate string resource.
    Why: Careless oversight. Implemented per suggestion in code review.
    
    Resolves: Udacity review comment #6

--- app/src/free/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity implements EndpointsAsyncTas
 
         // Set up the interstitial ad
         mInterstitialAd = new InterstitialAd(this);
-        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        mInterstitialAd.setAdUnitId(getString(R.string.banner_ad_unit_id));
     }
 
     @Override
