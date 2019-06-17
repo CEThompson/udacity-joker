a53b8347b0be51bf491efd386d41abbcea1d00c2
Christopher Thompson <ChristopherErikThompson@Gmail.com>
2019-06-17 10:42:35 -0700

    fix: add correct assertions to paid flavor of main activity async test
    What: The tests were missing validation. Assertions were adding checking that the async task does not return a null or empty string. Tests were verified to fail and pass in appropriate conditions.
    Why: Implemented per code review.

--- app/src/androidTestPaid/java/com/udacity/gradle/builditbigger/MainActivityPaidTest.java
+++ app/src/androidTestPaid/java/com/udacity/gradle/builditbigger/MainActivityPaidTest.java
@@ -2,6 +2,7 @@ package com.udacity.gradle.builditbigger;
 
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -10,6 +11,8 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertNotNull;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityPaidTest {
@@ -19,15 +22,19 @@ public class MainActivityPaidTest {
             = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void clickJokeButton_startsAsyncTask(){
-        // Clicking joke button starts the async task
+    public void clickingJokeButton_shouldGetJoke(){
+        /* 1. Click the joke button to trigger the async task:
+        *     Note this triggers espresso to wait
+        *     for the async task to complete */
         onView(withId(R.id.joke_button)).perform(click());
-    }
 
-    @Test
-    public void afterIdling_shouldDisplayJoke(){
+        /* 2. Retrieve the joke from the activity */
         String retrievedJoke = mRecipeActivityTestRule.getActivity().mJoke;
-        assert(retrievedJoke != null && !retrievedJoke.equals(""));
+        Log.d("MainActivityPaidTest", "retrievedJoke is: " + retrievedJoke);
+
+        /* 3. Assert the joke is neither null or empty */
+        assertNotNull(retrievedJoke);
+        assertFalse(retrievedJoke.isEmpty());
     }
 
 }
